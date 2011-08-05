d305dd6f9db858850de94d69c6e301ab5bc54c97
Phil Cohen <github@phlippers.net>
2011-08-04 22:42:14 -0700

    add initial sprockets manifest

---
+++ src/assets/javascripts/application.js
@@ -0,0 +1,14 @@
+// NOTE: This is just the Sprockets manifest file
+//
+//= require "codemirror"
+//
+//= require "jquery-1.6.2.min"
+//= require "jquery-ui-1.8.9.custom.min"
+//
+//= require "parse_uri"
+//
+//= require "runner"
+//= require "editor"
+//
+//= require "pen"
+//= require "hsv"
